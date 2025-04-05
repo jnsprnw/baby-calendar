d5bf8f32b484feac301d0cb4ed3916bb84eb1a26
Jonas Parnow <6gqrbj@posteo.org>
2025-04-05 19:33:50 +0200

    Remove duplicates

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ import (
 	"github.com/rs/cors"
 )
 
-const version = "0.1.13"
+const version = "0.1.14"
 const port = 8080
 
 // Global verfügbare timePeriods - werden nur einmal beim Serverstart geladen
